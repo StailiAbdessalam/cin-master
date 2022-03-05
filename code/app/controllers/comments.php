a71f1f0064319a54ebe24af17ba13882eb633237
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-03-05 14:00:15 +0100

    .

--- code/app/controllers/comments.php
+++ code/app/controllers/comments.php
@@ -1,15 +1,9 @@
-<?php 
+<?php
 require_once "../models/Database.php";
- 
-if(isset($_POST['content'])){
+if (isset($_POST['content'])) {
     session_start();
-    $_POST["user_id"]=$_SESSION['id'];
-
+    $_POST["user_id"] = $_SESSION['id'];
     $new_comment = new DataName("comments");
     $new_comment->insert($_POST);
     header("Location:../../public/pages/post.php");
- 
-}  
- 
- 
- 
+}
