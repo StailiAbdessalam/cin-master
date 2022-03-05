af30304badd708d4af099cd7169efce9185f7fcb
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-03-05 14:00:23 +0100

    .

--- code/app/controllers/Login.php
+++ code/app/controllers/Login.php
@@ -1,18 +1,14 @@
 <?php
 require_once "../../app/models/Database.php";
 session_start();
-
-
-
 if (isset($_POST['submit'])) {
     $email = $_POST['Gmail'];
     $Password = $_POST['Password'];
     $correct = false;
     $log = new DataName("user_");
     $user = $log->selectByEmail($email);
-
-    if($user && $user["password"] === $_POST["Password"]){
-        if(!isset($_SESSION)){
+    if ($user && $user["password"] === $_POST["Password"]) {
+        if (!isset($_SESSION)) {
             session_start();
         }
         $_SESSION["img"] = $user["P_prophile"];
@@ -22,7 +18,5 @@ if (isset($_POST['submit'])) {
         header("location:../../public/pages/index.php");
         return;
     }
-    
     header("location:../../public/pages/login.php");
 }
-
