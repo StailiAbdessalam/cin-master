695e098de80e9f1f9abd94e1ae27f74752a53251
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-03-10 15:07:26 +0100

    .

--- code/app/controllers/dashbord.php
+++ code/app/controllers/dashbord.php
@@ -8,28 +8,24 @@ function array_remove($selections, $arr)
     }
     return $result;
 }
-if (isset($_POST['submit'], $_FILES['image_url'])) {
 
+if (isset($_POST['submit'], $_FILES['image_url'])) {
     $imag_name = $_FILES['image_url']['name'];
     $imag_size = $_FILES['image_url']['size'];
     $tmp_name = $_FILES['image_url']['tmp_name'];
-    if ($imag_size > 1250000) {
-        echo "sorry , your file is too large ";
+    $img_ex = pathinfo($imag_name, PATHINFO_EXTENSION);
+    $img_ex_lc = strtolower($img_ex);
+    $allowed_exs = array("jpg", "jpeg", "png");
+    if (in_array($img_ex_lc, $allowed_exs)) {
+        $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+        $img_upload_path = '../image_user/' . $new_img_name;
+        move_uploaded_file($tmp_name, $img_upload_path);
+        $_POST['image_url'] = $new_img_name;
+        $N_film = new DataName("film");
+        $N_film->insert((array_remove(["submit"], $_POST)));
+        header("location:../../public/pages/index.php");
     } else {
-        $img_ex = pathinfo($imag_name, PATHINFO_EXTENSION);
-        $img_ex_lc = strtolower($img_ex);
-        $allowed_exs = array("jpg", "jpeg", "png");
-        if (in_array($img_ex_lc, $allowed_exs)) {
-            $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
-            $img_upload_path = '../image_user/' . $new_img_name;
-            move_uploaded_file($tmp_name, $img_upload_path);
-            $_POST['image_url'] = $new_img_name;
-            $N_film = new DataName("film");
-            $N_film->insert((array_remove(["submit"], $_POST)));
-            header("location:../../public/pages/index.php");
-        } else {
-            echo "you can't upload files of this type ";
-        }
+        echo "you can't upload files of this type ";
     }
 }
 $film = new DataName('film');
