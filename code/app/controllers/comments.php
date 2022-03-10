84f71cb2bf9ed99b99a4ed0e470375231aa696e7
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-03-10 15:07:20 +0100

    donner les commentaires son refrecher l apages

--- code/app/controllers/comments.php
+++ code/app/controllers/comments.php
@@ -1,9 +1,11 @@
 <?php
-require_once "../models/Database.php";
-if (isset($_POST['content'])) {
-    session_start();
-    $_POST["user_id"] = $_SESSION['id'];
-    $new_comment = new DataName("comments");
-    $new_comment->insert($_POST);
-    header("Location:../../public/pages/post.php");
-}
+// require_once "../models/Database.php";
+// if (isset($_POST['content'])) {
+//     session_start();
+//     $_POST["user_id"] = $_SESSION['id'];
+//     $new_comment = new DataName("comments");
+//     $new_comment->insert($_POST);
+//     header("Location:../../public/pages/post.php");
+// }
+
+var_dump($_POST);
