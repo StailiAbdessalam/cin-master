48178302937e678e61e0ae9e40a5ba24b91227d6
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-03-11 14:38:14 +0100

    .

--- code/app/controllers/comments.php
+++ code/app/controllers/comments.php
@@ -1,12 +1,15 @@
 <?php
 require_once "../models/Database.php";
-
-    // session_start();
-    // $_POST["user_id"] = $_SESSION['id'];
-    $new_comment = new DataName("comments");
-    $new_comment->insert($_POST);
-    // header("Location:../../public/pages/post.php");
-
-
-
-
+if(!isset($_SESSION)){
+    session_start();
+}
+// $_POST["user_id"] = $_SESSION['id'];
+$new_comment = new DataName("comments");
+$new_comment->insert($_POST);
+// header("Location:../../public/pages/post.php);
+$getcomm = new DataName("comments");
+$newComment = $getcomm->getcomment();
+// $newComment = json_encode($newComment);
+$user= ["img" => $_SESSION["img"], "nom" => $_SESSION["nom"], "prenom" => $_SESSION["prenom"]];
+$data = ["comment" => $newComment, "user" => $user];
+echo json_encode($data); 
